92a97c99ecafd2b7372ac7212e9a52a48f0625af
Dumitru Uzun <contact@duzun.me>
2015-04-15 09:48:39 +0300

    page for github.io

---
+++ example.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$('.showmore_one').showMore({
+		speedDown: 300,
+	        speedUp: 300,
+	        height: '165px',
+	        showText: 'Show more',
+	        hideText: 'Show less'
+	});
+
+	$('.showmore_two').showMore({
+		speedDown: 300,
+	        speedUp: 300,
+	        height: '195px',
+	        showText: 'Show more',
+	        hideText: 'Show less'
+	});
+
+});
